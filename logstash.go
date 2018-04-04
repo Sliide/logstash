47206f796bc85af525d85ed34c404460af7b7749
Dean Elbaz <elbaz.dean@gmail.com>
2018-04-03 19:51:17 -0700

    add log rotation

--- logstash.go
+++ logstash.go
@@ -1,21 +1,32 @@
 package logstash
 
 import (
+	"io"
 	"os"
+	"os/exec"
+	"time"
 
 	log "github.com/sirupsen/logrus"
-	"io"
 )
 
-var LogLevels = map[string]log.Level{
-	"DEBUG":   log.DebugLevel,
-	"INFO":    log.InfoLevel,
-	"WARNING": log.WarnLevel,
-	"ERROR":   log.ErrorLevel,
-	"FATAL":   log.FatalLevel,
-	"PANIC":   log.PanicLevel,
-}
+var (
+	LogLevels = map[string]log.Level{
+		"DEBUG":   log.DebugLevel,
+		"INFO":    log.InfoLevel,
+		"WARNING": log.WarnLevel,
+		"ERROR":   log.ErrorLevel,
+		"FATAL":   log.FatalLevel,
+		"PANIC":   log.PanicLevel,
+	}
 
+	rotationCheckInterval       = 10 * time.Second
+	maxLogFileSize        int64 = 5 << 30 // 5GiB
+
+	stopRotation chan struct{}
+)
+
+// Init sets up logging
+// This function should only be called once when the service is started
 func Init(logLevel string, logFileName string, env string, service string) bool {
 
 	logFile, err := os.Create(logFileName)
@@ -30,8 +41,86 @@ func Init(logLevel string, logFileName string, env string, service string) bool 
 	})
 
 	log.SetLevel(LogLevels[logLevel])
-	mw := io.MultiWriter(os.Stdout, logFile)
-
-	log.SetOutput(mw)
+	setLogFile(logFile)
+	go rotate(logFileName)
 	return true
+}
+
+func setLogFile(writer io.Writer) {
+	log.SetOutput(io.MultiWriter(os.Stdout, writer))
+}
+
+// rotate checks periodically if the
+// when the current logfile
+func rotate(logFileName string) {
+	log.Debug("starting rotation for", logFileName)
+
+	if stopRotation != nil {
+		log.Debug("stopping rotation")
+		close(stopRotation)
+	}
+	stopRotation = make(chan struct{})
+
+	ticker := time.NewTicker(rotationCheckInterval)
+	for {
+		select {
+		case <-stopRotation:
+			log.Debug("stopping rotation for", logFileName)
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			f, err := os.Stat(logFileName)
+			if f.Size() > maxLogFileSize {
+				log.Debugf("log file too large, rotating the log file")
+				rotatedFilename := logFileName + ".1"
+
+				// if the rotated file already exist, add timestamp to its name and archive it
+				if _, err = os.Stat(rotatedFilename); err == nil {
+
+					err = delayedCompression(rotatedFilename)
+					if err != nil {
+						// if the renaming for compression failed, try again before rotating
+						continue
+					}
+				}
+
+				err = os.Rename(logFileName, rotatedFilename)
+				if err != nil {
+					log.Error("couldn't rename log file", logFileName, err)
+					continue
+				}
+
+				NewLogFile, err := os.Create(logFileName)
+				if err != nil {
+					panic(err)
+				}
+
+				setLogFile(NewLogFile)
+			}
+		}
+
+	}
+}
+
+// Rename and compress old logs
+func delayedCompression(rotatedFilename string) error {
+
+	timestampedName := rotatedFilename + time.Now().Format(time.RFC3339)
+
+	log.Debugf("found previous log file %s, renaming to %s", rotatedFilename, timestampedName)
+
+	err := os.Rename(rotatedFilename, timestampedName)
+	if err != nil {
+		log.Error("couldn't rename rotated log file", rotatedFilename, err)
+		return err
+	}
+	cmd := exec.Command("gzip", timestampedName)
+	go func() {
+		log.Debug("Compressing log file", timestampedName)
+		err := cmd.Run()
+		if err != nil {
+			log.Error("gzip on rotated log file failed", timestampedName, err)
+		}
+	}()
+	return nil
 }
--- logstash_test.go
+++ logstash_test.go
@@ -1,20 +1,30 @@
-package logstash_test
+package logstash
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
+	"time"
 
 	. "gopkg.in/check.v1"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/sliide/logstash"
 )
 
 func TestLogstash(t *testing.T) { TestingT(t) }
 
 type LogstashTestSuite struct{}
+
+type LogMessage struct {
+	Message string `json:"message"`
+	Level   string `json:"level"`
+	MyField string `json:"my_field"`
+	LineNo  string `json:"lineno"`
+	Logger  string `json:"logger"`
+}
 
 var _ = Suite(
 	&LogstashTestSuite{},
@@ -22,8 +32,9 @@ var _ = Suite(
 
 func (s *LogstashTestSuite) TestFileLogger(c *C) {
 	var filename = "./logs"
+	defer os.Remove(filename)
 
-	logstash.Init(
+	Init(
 		"INFO",
 		filename,
 		"local",
@@ -32,17 +43,8 @@ func (s *LogstashTestSuite) TestFileLogger(c *C) {
 
 	log.WithFields(log.Fields{"my_field": fmt.Sprintf("%d", 1)}).Error("This is an error message")
 
-	type LogMessage struct {
-		Message string `json:"message"`
-		Level   string `json:"level"`
-		MyField string `json:"my_field"`
-		LineNo  string `json:"lineno"`
-		Logger  string `json:"logger"`
-	}
-
 	jsonString, err := ioutil.ReadFile(filename)
 
-	fmt.Println(string(jsonString))
 	if err != nil {
 		c.Fail()
 	}
@@ -58,4 +60,56 @@ func (s *LogstashTestSuite) TestFileLogger(c *C) {
 	c.Assert(logMessage.Message, Equals, "This is an error message")
 	c.Assert(logMessage.LineNo, Matches, ".*logstash_test.go.*")
 	c.Assert(logMessage.Logger, Matches, ".*TestFileLogger")
+}
+
+func (s *LogstashTestSuite) TestFileRotation(c *C) {
+	logsDir := "./test_logs"
+	os.Mkdir(logsDir, os.ModePerm)
+	logfile := logsDir + "/log"
+	defer os.RemoveAll(logsDir)
+
+	rotationCheckInterval = 100 * time.Millisecond
+	maxLogFileSize = 1000
+
+	Init(
+		"DEBUG",
+		logfile,
+		"local",
+		"logstash",
+	)
+
+	stop := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				log.Info("the cake is a lie")
+				time.Sleep(10 * time.Millisecond)
+			}
+		}
+	}()
+
+	time.Sleep(210 * time.Millisecond)
+	close(stop)
+
+	dir, err := os.Open(logsDir)
+	c.Assert(err, IsNil)
+
+	files, err := dir.Readdir(100)
+	c.Assert(err, IsNil)
+	c.Assert(len(files), Equals, 3)
+
+	for _, f := range files {
+		switch f.Name() {
+		case "log":
+			c.Assert(f.Size() < 1000, Equals, true)
+		case "log.1":
+			c.Assert(f.Size() > 1000, Equals, true)
+		default:
+			c.Assert(strings.HasSuffix(f.Name(), ".gz"), Equals, true)
+		}
+	}
 }
